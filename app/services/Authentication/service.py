766d70b463f8673feb1010a495420b3c60a11da9
VannWasHere <ateen1203@gmail.com>
2024-05-27 15:59:17 +0700

    Configure request and query from creating account

--- app/services/Authentication/service.py
+++ app/services/Authentication/service.py
@@ -1,4 +1,21 @@
-from . import response_schema
+import uuid
+
+from . import response_schema as Response
+from utils.database import create_connection
+from fastapi.responses import JSONResponse
 
 def register(request):
-    return response_schema.UserCreated(message="jasbdhuysabutdrcv")
+    conn = create_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            """
+            INSERT INTO users (id, username, password, first_name, last_name, dob, roles, gender)
+            VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+            """, (str(uuid.uuid4()), request.username, request.password, request.first_name, request.last_name, request.dob, request.roles, request.gender)
+        )
+        conn.commit()
+        conn.close()
+        return JSONResponse({"message": "Account has beed created"}, status_code=201)
+    except Exception as err:
+        return JSONResponse({"message": "Failed creating an account", "err": err}, status_code=500)
