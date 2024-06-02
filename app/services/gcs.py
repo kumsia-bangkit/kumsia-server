676052d6ff2f493b6e61c2f0f77c53bf343aa1bb
dianisa <dianisawulandari@gmail.com>
2024-06-02 16:48:44 +0700

    chore: setup cloud storage

---
+++ app/services/gcs.py
@@ -0,0 +1,15 @@
+import os
+from google.cloud import storage
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'cert/kumsia-key.json'
+class GCStorage:
+    def __init__(self):
+        self.storage_client = storage. Client()
+        self.bucket_name = 'kumsia-storage'
+
+    def upload_file(self, file):
+        bucket = self.storage_client.get_bucket(self.bucket_name)
+        file_path = file.filename
+        blob = bucket.blob(file_path)
+        blob.upload_from_file(file.file, content_type='image/jpeg')
+        return f'https://storage.cloud.google.com/{self.bucket_name}/{file_path}'
