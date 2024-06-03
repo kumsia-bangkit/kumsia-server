4e1e5b377348e94b928c6639e10da960c32f0542
VannWasHere <ateen1203@gmail.com>
2024-06-03 11:15:15 +0700

    refactor: change project structure and fix path

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source ../env/Scripts/activate
+source ./env/Scripts/activate
 
 # Run the Uvicorn server with the specified host and port
-uvicorn main:app --reload --host 0.0.0.0 --port 2134
+uvicorn app.main:app --reload --host 0.0.0.0 --port 2134
