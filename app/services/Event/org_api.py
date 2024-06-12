dab8337d67a808a59480eaaac3a7c68dc4dd342e
dianisa <dianisawulandari@gmail.com>
2024-06-12 14:14:13 +0700

    fix: fix acces token parameter for event api

--- app/services/Event/org_api.py
+++ app/services/Event/org_api.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, File, Form, UploadFile
+from typing import Annotated
+from fastapi import APIRouter, File, Form, Header, UploadFile
 from . import org_service as EventService, request_schema, response_schema
 from app.services.gcs import GCStorage
 from app.utils.authentication import validate_token_and_id
@@ -6,12 +7,12 @@ from app.utils.authentication import validate_token_and_id
 org_event_router = APIRouter(prefix="/events/org", tags=['Event Organization'])
 
 @org_event_router.get('/all', response_model=response_schema.OrganizationEventList)
-async def get_events(access_token: str):
+async def get_events(access_token: Annotated[str, Header()]):
     org_id = validate_token_and_id(access_token)
     return EventService.get_all(org_id)
 
 @org_event_router.get('/{event_id}', response_model=response_schema.OrganizationEvent)
-async def get_event_by_id(event_id: str, access_token: str):
+async def get_event_by_id(event_id: str, access_token: Annotated[str, Header()]):
     org_id = validate_token_and_id(access_token)
     return EventService.get_event_by_id(event_id, org_id)
 
@@ -24,7 +25,7 @@ async def post_event(event:request_schema.Event=Form(...), file:UploadFile=File(
     return EventService.post_event(event, file_path, org_id)
 
 @org_event_router.put('/update/{event_id}', response_model=response_schema.OrganizationEvent)
-async def update_event(event_id:str, event: request_schema.Event, file:UploadFile=File(...), access_token:str=Form(...)):
+async def update_event(access_token: Annotated[str, Header()], event_id:str, event: request_schema.Event, file:UploadFile=File(...)):
     org_id = validate_token_and_id(access_token)
     file_path = None
     if file:
@@ -33,16 +34,16 @@ async def update_event(event_id:str, event: request_schema.Event, file:UploadFil
     return EventService.update_event(event_id, event, file_path, org_id)
 
 @org_event_router.delete('/delete')
-async def delete_event(event_id: str, access_token: str):
+async def delete_event(event_id: str, access_token: Annotated[str, Header()]):
     org_id = validate_token_and_id(access_token)
     return EventService.delete_event(event_id, org_id)
 
 @org_event_router.put('/submit', response_model=response_schema.OrganizationEvent)
-async def submit_event(event_id: str, access_token: str):
+async def submit_event(event_id: str, access_token: Annotated[str, Header()]):
     org_id = validate_token_and_id(access_token)
     return EventService.submit_event(event_id, org_id)
 
 @org_event_router.put('/cancel', response_model=response_schema.OrganizationEvent)
-async def cancel_event(event_id: str, access_token: str):
+async def cancel_event(event_id: str, access_token: Annotated[str, Header()]):
     org_id = validate_token_and_id(access_token)
     return EventService.cancel_event(event_id, org_id)
