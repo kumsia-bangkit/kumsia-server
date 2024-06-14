67f2e61c306ba08f2517d9151090a81fd38bdcc1
dianisa <dianisawulandari@gmail.com>
2024-06-14 12:46:51 +0700

    feat: order master data

--- app/services/MasterData/service.py
+++ app/services/MasterData/service.py
@@ -6,13 +6,13 @@ conn = create_connection()
 cur = conn.cursor()
 
 def get_master_hobby():
-    cur.execute("SELECT * FROM master_hobby;")
+    cur.execute("SELECT * FROM master_hobby ORDER BY hobby ASC;")
     hobby = [h["hobby"] for h in cur.fetchall()]
 
     return response_schema.HobbyList(Hobbies=hobby)
 
 def get_master_city():
-    cur.execute("SELECT * FROM master_city;")
+    cur.execute("SELECT * FROM master_city ORDER BY city ASC;")
     city = [c["city"] for c in cur.fetchall()]
 
     return response_schema.CityList(Cities=city)
