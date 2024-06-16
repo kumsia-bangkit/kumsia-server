01dad37c9f071cb2185265150c9e96eef6563b2b
rizqy.rmdhn <rizqy.r@gmail.com>
2024-06-16 12:32:27 +0700

    feat: standardization and KNN model to get reccommended accounts

--- app/services/Reccommendation/account_service.py
+++ app/services/Reccommendation/account_service.py
@@ -7,6 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 from datetime import datetime, date
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 conn = create_connection()
 cur = conn.cursor()
@@ -46,6 +48,7 @@ def get_recommendation(user_id: str):
     """
     # d29a683e-ce09-438d-9c13-2e452d0ae4e6
     query = f"""
+    SELECT * FROM (
     SELECT U.*, P.hobby
     FROM users U
     JOIN friend F ON U.user_id = F.first_party_id
@@ -57,12 +60,19 @@ def get_recommendation(user_id: str):
     JOIN friend F ON U.user_id = F.second_party_id
     LEFT JOIN preference P on U.preference_id = P.preference_id
     WHERE F.first_party_id != {user_id} OR F.second_party_id != {user_id}
+    UNION
+    SELECT U.*, P.hobby
+    FROM users U 
+    LEFT JOIN preference P on U.preference_id = P.preference_id
+    WHERE U.user_id = {user_id}
+    )
+    ORDER BY user_id;
     """
 
     cur.execute(query)
     non_friends = cur.fetchall()
 
-    # get user logged in and their preferences
+    # get user logged in's preferences
     query = f"""
     SELECT P.hobby, P.gender, P.city, P.religion
     FROM users U
@@ -82,9 +92,9 @@ def get_recommendation(user_id: str):
         df[hobby] = df.apply(lambda x:1 if hobby in x.hobby else 0, axis=1)
     
     # set gender match 
-    df["gender_match"] = df["gender"].apply(lambda x: 1 if x in user_preference["gender"] or len(user_preference["gender"]) == 0 else 0) 
+    df["gender_match"] = df["gender"].apply(lambda x: 4 if x in user_preference["gender"] or len(user_preference["gender"]) == 0 else 0) 
     # set city match
-    df["city_match"] = df["city"].apply(lambda x: 1 if x in user_preference["city"] or len(user_preference["city"]) == 0 else 0) 
+    df["city_match"] = df["city"].apply(lambda x: 2 if x in user_preference["city"] or len(user_preference["city"]) == 0 else 0) 
     # set religion match 
     df["religion_match"] = df["religion"].apply(lambda x: 1 if x in user_preference["religion"] or len(user_preference["religion"]) == 0 else 0) 
 
@@ -98,5 +108,36 @@ def get_recommendation(user_id: str):
 
     # Check if has profile picture
     df["has_profile_pic"] = df.apply(lambda x: 1 if x.profile_picture else 0)
+
+    # Drop unused columns
+    preprocessed_df = df.drop(["preference_id", "username", "name", "password", "email", "contact"
+                            "guardian_contact", "profile_picture", "dob", "last_activity", "hobby",
+                            "religion", "gender", "city"], axis=1)
+
+    # Standardization only on age and offline days
+    scaler = StandardScaler()
+    preprocessed_df[["age", "days_offline"]] = scaler.fit_transform(preprocessed_df[["age", "days_offline"]])
+
+    # Get User DF and drop user logged in from preprocessed df
+    user_df = preprocessed_df[preprocessed_df["user_id"] == user_id]
+    preprocessed_df.drop(preprocessed_df[preprocessed_df["user_id"] == user_id].index)
+
+    # separate id
+    id_df = preprocessed_df["user_id"]
+    preprocessed_df.drop(["user_id"], axis=1)
+
+    # Neighbours Model
+    n_neighbors = len(preprocessed_df) 
+    if len(preprocessed_df) > 15:
+        n_neighbors = 15
+
+    knn = NearestNeighbors(n_neighbors=n_neighbors, algorithm='auto').fit(preprocessed_df)
+    distances, indices = knn.kneighbors(user_df)
+    top_n_index = indices[0]
+    top_n_user = df.iloc[top_n_index]
+
+    # if more than 5 reccommended accounts, pick random 5
+    if n_neighbors > 5:
+        top_n_user = top_n_user.sample(5)
 
     return
