0a5d27a80ceb2c38c44285ea5fb5f0ec3269e2ed
rizqy.rmdhn <rizqy.r@gmail.com>
2024-06-18 21:39:57 +0700

    fix: add users returned attribute to have dob and hobbies

--- app/services/Recommendation/response_schema.py
+++ app/services/Recommendation/response_schema.py
@@ -5,10 +5,12 @@ class User(BaseModel):
     user_id: Optional[str]
     username: Optional[str]
     name: Optional[str]
+    dob: Optional[str]
     profile_picture: Optional[str]
     religion: Optional[str]
     gender: Optional[str]
     city: Optional[str]
+    hobbies: Optional[List[str]]
 
 class UsersList(BaseModel):
     users: Optional[List[User]]
