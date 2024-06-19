c22c7a9b2e0f02f1f928f46b64877499ca0e5938
rizqy.rmdhn <rizqy.r@gmail.com>
2024-06-19 14:54:49 +0700

    fix query and preprocessing

--- app/services/Recommendation/account_service.py
+++ app/services/Recommendation/account_service.py
@@ -13,22 +13,19 @@ def get_recommendation(user_id: str):
     # Get Non friends
     query = f"""
     SELECT * FROM (
-    SELECT U.*, P.hobby
-    FROM users U
-    JOIN friend F ON U.user_id = F.first_party_id
-    LEFT JOIN preference P on U.preference_id = P.preference_id
-    WHERE F.first_party_id != '{user_id}' OR F.second_party_id != '{user_id}'
-    UNION
-    SELECT U.*, P.hobby 
-    FROM users U 
-    JOIN friend F ON U.user_id = F.second_party_id
-    LEFT JOIN preference P on U.preference_id = P.preference_id
-    WHERE F.first_party_id != '{user_id}' OR F.second_party_id != '{user_id}'
-    UNION
-    SELECT U.*, P.hobby
-    FROM users U 
-    LEFT JOIN preference P on U.preference_id = P.preference_id
-    WHERE U.user_id = '{user_id}'
+        SELECT U.*, P.hobby
+        FROM users U
+        LEFT JOIN preference P on U.preference_id = P.preference_id
+        WHERE NOT EXISTS (
+            SELECT F.first_party_id, F.second_party_id
+            FROM friend F
+            WHERE (U.user_id = F.first_party_id AND F.second_party_id = '{user_id}') OR (U.user_id = F.second_party_id AND F.first_party_id = '{user_id}')
+        )
+        UNION
+        SELECT U.*, P.hobby
+        FROM users U 
+        LEFT JOIN preference P on U.preference_id = P.preference_id
+        WHERE U.user_id = '{user_id}'
     )
     ORDER BY user_id;
     """
@@ -41,7 +38,7 @@ def get_recommendation(user_id: str):
     SELECT P.hobby, P.gender, P.city, P.religion
     FROM users U
     JOIN preference P on U.preference_id = P.preference_id
-    WHER u.user_id = '{user_id}'
+    WHERE u.user_id = '{user_id}'
     """
     cur.execute(query)
     user_preference = cur.fetchone()
@@ -50,7 +47,7 @@ def get_recommendation(user_id: str):
     df = pd.DataFrame.from_dict(non_friends)
 
     # get hobbies
-    hobbies = get_master_hobby()
+    hobbies = get_master_hobby().model_dump().get("Hobbies")
     # one hot encode all hobbies
     for hobby in hobbies:
         df[hobby] = df.apply(lambda x:1 if hobby in x.hobby else 0, axis=1)
@@ -71,12 +68,12 @@ def get_recommendation(user_id: str):
     df["days_offline"] = df.apply(lambda x: (datetime.today() - x.last_activity).days, axis=1)
 
     # Check if has profile picture
-    df["has_profile_pic"] = df.apply(lambda x: 1 if x.profile_picture else 0)
+    df["has_profile_pic"] = df.apply(lambda x: 1 if x.profile_picture else 0, axis=1)
 
     # Drop unused columns
     preprocessed_df = df.drop(["preference_id", "username", "name", "password", "email", "contact"
                             "guardian_contact", "profile_picture", "dob", "last_activity", "hobby",
-                            "religion", "gender", "city"], axis=1)
+                            "religion", "gender", "city", "is_new_user"], axis=1)
 
     # Standardization only on age and offline days
     scaler = StandardScaler()
@@ -88,7 +85,7 @@ def get_recommendation(user_id: str):
 
     # separate id
     id_df = preprocessed_df["user_id"]
-    preprocessed_df.drop(["user_id"], axis=1)
+    preprocessed_df = preprocessed_df.drop(["user_id"], axis=1)
 
     # Neighbours Model
     n_neighbors = len(preprocessed_df) 
@@ -96,7 +93,7 @@ def get_recommendation(user_id: str):
         n_neighbors = 15
 
     knn = NearestNeighbors(n_neighbors=n_neighbors, algorithm='auto').fit(preprocessed_df)
-    distances, indices = knn.kneighbors(user_df)
+    distances, indices = knn.kneighbors(user_df.drop(["user_id"], axis=1))
     top_n_index = indices[0]
     top_n_user = df.iloc[top_n_index]
 
@@ -109,7 +106,7 @@ def get_recommendation(user_id: str):
         recommended_users.append(response_schema.User(user_id=row["user_id"],
                                                       username=row["username"],
                                                       name=row["name"],
-                                                      dob=row["dob"],
+                                                      dob=row["dob"].strftime("%Y-%m-%d"),
                                                       profile_picture=row["profile_picture"],
                                                       religion=row["religion"],
                                                       gender=row["gender"],
