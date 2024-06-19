9878b4ba40d5d4d1f2db06a20b8df9dcf9397c8c
ictdev <ict.dev@infracom-tech.com>
2024-06-19 18:30:39 +0700

    feat: adding update last activity

--- app/services/Interaction/like_api.py
+++ app/services/Interaction/like_api.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Header
 from app.utils.authentication import validate_token_and_id
+from app.utils.utility import update_last_activity
 from . import like_service as LikeService
 
 like_router = APIRouter(prefix="/like", tags=['Like'])
@@ -13,9 +14,11 @@ async def get_is_liked(event_id: str, access_token: Annotated[str, Header()]):
 @like_router.post('/create')
 async def like_event(event_id: str, access_token: Annotated[str, Header()]):
     user_id = validate_token_and_id(access_token)
+    update_last_activity(user_id)
     return LikeService.post_like(event_id, user_id)
 
 @like_router.delete('/delete')
 async def unlike_event(event_id: str, access_token: Annotated[str, Header()]):
     user_id = validate_token_and_id(access_token)
+    update_last_activity(user_id)
     return LikeService.delete_like(event_id, user_id)
